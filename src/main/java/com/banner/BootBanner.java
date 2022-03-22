44b5457fe31fd510fb70f2eb15cfe71bf0ed8b6d
eunhye park <lime1pm@naver.com>
2022-03-22 19:23:19 +0900

    Fix: BootBanner extends Banner
    BootBanner 가 Banner 상속하도록 수정

--- src/main/java/com/banner/BootBanner.java
+++ src/main/java/com/banner/BootBanner.java
@@ -1,17 +1,16 @@
 package com.banner;
 
-import java.io.IOException;
-
-public class BootBanner{
+public class BootBanner extends Banner{
     private static final String FILE_NAME = "banner.txt";
-    private Banner banner = new Banner(FILE_NAME);
+//    private Banner banner = new Banner(FILE_NAME);
 
-    public String display() throws IOException {
-        String output = banner.display();
-        return output+" 버전1";
+    public BootBanner() {
+        super(FILE_NAME);
     }
 
-    public void update(String content){
+//    public String display() throws IOException {
+//        String output = banner.display();
+//        return output+" 버전1";
+//    }
 
-    }
 }
