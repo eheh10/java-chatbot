23eba0c90e3fcbbe6f570d13f75c2bdc8ddbf4b1
eunhye park <lime1pm@naver.com>
2022-03-23 18:03:46 +0900

    Refactor: DateCommand
    DateCommand 리팩토링

--- src/main/java/com/command/DateCommand.java
+++ src/main/java/com/command/DateCommand.java
@@ -8,72 +8,81 @@ import java.util.Arrays;
 public class DateCommand {
 
     public static void action(ArrayList<String> command){
-        LocalDate today = LocalDate.now();
+        LocalDate date = LocalDate.now();
+        int dayNum = 0;
 
-        if (command.size() == 0){
-            System.out.println(today);
-        }else if(command.size() == 1){
-            if (checkNumber(command.get(0))){
-                int dayNum = Integer.parseInt(command.get(0));
-                System.out.println(today.plusDays(dayNum));
-            }else{
-                return;
-            }
-        }else if(Arrays.asList("c","cal","calculate").contains(command.get(0))){
-            String meta = command.get(1);
-            LocalDate date = today;
+        if (command.size() > 0) {
+            if (isNumber(command.get(0))) {
+                dayNum = Integer.parseInt(command.get(0));
+            } else if (command.size() > 1 && Arrays.asList("c", "cal", "calculate").contains(command.get(0))) {
+                String meta = command.get(1);
+                boolean defaultDay = meta.equals("+")||meta.equals("-");
+                String op = defaultDay?meta:"";
 
-            if (meta.equals("+") || meta.equals("-")){
-                command.add(1,date.format(DateTimeFormatter.BASIC_ISO_DATE));
-            }else{
-                try{
-                    date = LocalDate.parse(meta, DateTimeFormatter.BASIC_ISO_DATE); //yyyyMMdd
-                }catch (Exception e){
-                    System.out.println("error: "+meta+"는 올바르지 않은 포맷입니다.\n" +
+                if (!defaultDay) {
+                    try {
+                        date = LocalDate.parse(meta, DateTimeFormatter.BASIC_ISO_DATE); //yyyyMMdd
+                    } catch (Exception e) {
+                        System.out.println("error: " + meta + "는 올바르지 않은 포맷입니다.\n" +
                             "다음과 같이 포맷 양식을 지켜주세요.\n" +
                             "`yyyyMMdd 패턴으로 받습니다.`");
-                    return;
-                }
-            }
+                        return;
+                    }
 
-            int dayNum = 0;
-            if (command.size()==4){
-                if (checkNumber(command.get(3))){
-                    dayNum = Integer.parseInt(command.get(3));
-                }else{
-                    return;
-                }
-            }
+                    if(command.size() > 2){
+                        op = command.get(2);
+                    }else{
+                        System.out.println("error: " + String.join(" ", command) + "는 올바르지 않은 메타 데이터입니다. \n" +
+                            "다음의 메타 데이터를 지켜주세요.\n" +
+                            "[c|cal|calculate] [|txt:yyyyMMdd] [+|-] [|num:days]"
+                        );
+                    }
 
-            String op = command.get(2);
-            if(op.equals("-")){
-                dayNum = dayNum * -1;
-            }else if(!op.equals("+")){
-                System.out.println("error: `"+op+"`는 올바르지 않은 메타데이터 입니다.\n" +
+                    if (command.size()==4) {
+                        if (isNumber(command.get(3))){
+                            dayNum = Integer.parseInt(command.get(3));
+                        }else {
+                            System.out.println("error: "+command.get(3)+"는 올바르지 않은 포맷입니다.\n" +
+                                "다음과 같이 포맷 양식을 지켜주세요.\n" +
+                                "`숫자 데이터만 가능합니다.`"
+                            );
+                            return;
+                        }
+                    }
+                }
+
+                if (op.equals("-")) {
+                    dayNum = dayNum * -1;
+                } else if (!op.equals("+")) {
+                    System.out.println("error: `" + op + "`는 올바르지 않은 메타데이터 입니다.\n" +
                         "다음의 메타 데이터중 하나를 선택해주세요.\n" +
-                        "[+|-]");
-                return;
-            }
-            System.out.println(date.plusDays(dayNum).format(DateTimeFormatter.ISO_LOCAL_DATE)); //yyyy-MM-dd
-
-        }else{
-            System.out.println("error: "+String.join(" ",command)+"는 올바르지 않은 메타 데이터입니다. \n" +
+                        "[+|-]"
+                    );
+                    return;
+                }
+            } else {
+                System.out.println("error: " + String.join(" ", command) + "는 올바르지 않은 메타 데이터입니다. \n" +
                     "다음의 메타 데이터를 지켜주세요.\n" +
                     "[|txt:addNumber]\n" +
-                    "[c|cal|calculate] [|txt:yyyyMMdd] [+|-] [|num:days]");
-            return;
+                    "[c|cal|calculate] [|txt:yyyyMMdd] [+|-] [|num:days]"
+                );
+                return;
+            }
         }
+        System.out.println(date.plusDays(dayNum).format(DateTimeFormatter.ISO_LOCAL_DATE)); //yyyy-MM-dd
     }
 
-    static boolean checkNumber(String data){
-        try {
-            Integer.parseInt(data);
-        }catch (Exception e){
-            System.out.println("error: "+data+"는 올바르지 않은 포맷입니다.\n" +
-                    "다음과 같이 포맷 양식을 지켜주세요.\n" +
-                    "`숫자 데이터만 가능합니다.`");
-            return false;
+    static boolean isNumber(String data){
+        if (data.charAt(0)=='-'||data.charAt(0)=='+'){
+            data = data.substring(1);
+        }
+
+        for(char c:data.toCharArray()){
+            if (!Character.isDigit(c)){
+                return false;
+            }
         }
         return true;
     }
+
 }
