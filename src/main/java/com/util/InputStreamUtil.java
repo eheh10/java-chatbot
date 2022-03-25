4664eac811ae06e67c0b66067a672287194f3244
eunhye park <lime1pm@naver.com>
2022-03-26 03:44:28 +0900

    Feat: Add InputStreamUtil class
    InputStreamUtil class 추가

---
+++ src/main/java/com/util/InputStreamUtil.java
@@ -0,0 +1,22 @@
+package com.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class InputStreamUtil {
+    private InputStreamUtil(){}
+
+    public static String read(InputStream is) throws IOException {
+        InputStreamReader rd = new InputStreamReader(is, StandardCharsets.UTF_8);
+        char[] cbuffer = new char[2];
+        int len=0;
+        StringBuilder output=new StringBuilder();
+
+        while((len=rd.read(cbuffer))!=-1){
+            output.append(cbuffer,0,len);
+        }
+        return output.toString();
+    }
+}
