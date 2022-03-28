7e86693007691d5fae452e700b144862994f4d89
eunhye park <lime1pm@naver.com>
2022-03-29 00:55:51 +0900

    Feat: Fix calling command action in Chatbot
    action 호출 방식 변경

--- src/main/java/com/main/Chatbot.java
+++ src/main/java/com/main/Chatbot.java
@@ -1,14 +1,12 @@
 package com.main;
 
 import com.banner.BootBanner;
-import com.command.CalCommand;
-import com.command.DateCommand;
-import com.command.FileCommand;
-import com.command.SystemCommand;
+import com.command.*;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class Chatbot {
 
@@ -28,22 +26,13 @@ public class Chatbot {
             String command = input[0];
             ArrayList<String> meta = new ArrayList<>(Arrays.asList(input).subList(1,input.length));
 
-            switch (command){
-                case "/system":
-                    SystemCommand.action(bootBanner,meta);
-                    break;
-                case "/cal":
-                    CalCommand.action(meta);
-                    break;
-                case "/date":
-                    DateCommand.action(meta);
-                    break;
-                case "/file":
-                    FileCommand.action(meta);
-                    break;
-                default:
-                    System.out.println("warning: `"+command+"`는 존재하지 않는 명령어 입니다.");
-            }
+            Command cmd = Stream.of(new SystemCommand(bootBanner), new CalCommand(), new DateCommand(), new FileCommand(), new ErrorCommand())
+                    .filter(_cmd -> _cmd.isSupport(command))
+                    .findFirst()
+                    .orElseThrow();
+
+            cmd.action(meta);
+
         }
     }
 }
